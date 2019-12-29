ba251ae2c6a971e9fa6513173dbd618aad9f6997
Cory Sanin <corysanin@outlook.com>
2019-12-28 23:39:22 -0600

    fix issues with snap package

--- gamestateIntegration.js
+++ gamestateIntegration.js
@@ -6,13 +6,13 @@ const os = require('os')
 const ipc = require('electron').ipcRenderer
 
 const REG = 'SteamPath'
-const WINSTEAMLIB = path.join('steamapps','libraryfolders.vdf')
+const WINSTEAMLIB = path.join('steamapps', 'libraryfolders.vdf')
 const GSIFILENAME = 'gamestate_integration_kitten.cfg'
-const HOMEDIR = os.homedir()
+const HOMEDIR = (process.env.SNAP) ? path.join('/home', process.env.USER) : os.homedir()
 
-function saveConfig(config){
+function saveConfig(config) {
   let cfg = VDF.stringify({
-    'Kitten State API Config':{
+    'Kitten State API Config': {
       'uri': config.url,
       'timeout': 5.0,
       'buffer': 0.1,
@@ -27,22 +27,22 @@ function saveConfig(config){
       }
     }
   })
-  getCSGOPath(function(pth){
+  getCSGOPath(function (pth) {
     fs.writeFile(path.join(pth, GSIFILENAME), cfg)
-    if(os.platform() === 'win32'){
+    if (os.platform() === 'win32') {
       ipc.send('csgoicon', path.resolve(pth, '..', '..', 'csgo.exe'))
     }
   })
 }
 
-function getCSGOPath(cb){
-  getLibraryFoldersVDFPath(function(vdfPath){
-    getLibraryFolders(vdfPath,function(libraries){
-      libraries.push(path.join(path.dirname(vdfPath),'common'))
-      libraries.forEach(function(pth,index){
+function getCSGOPath(cb) {
+  getLibraryFoldersVDFPath(function (vdfPath) {
+    getLibraryFolders(vdfPath, function (libraries) {
+      libraries.push(path.join(path.dirname(vdfPath), 'common'))
+      libraries.forEach(function (pth, index) {
         let cfgPath = path.join(pth, 'Counter-Strike Global Offensive', 'csgo', 'cfg')
-        fs.pathExists(cfgPath , function(err, exists){
-          if(!err && exists){
+        fs.pathExists(cfgPath, function (err, exists) {
+          if (!err && exists) {
             cb(cfgPath)
           }
         })
@@ -51,53 +51,53 @@ function getCSGOPath(cb){
   })
 }
 
-function getLibraryFoldersVDFPath(cb=function(){}){
-  if(os.platform() === 'win32'){
+function getLibraryFoldersVDFPath(cb = function () { }) {
+  if (os.platform() === 'win32') {
     //look at HKEY_CURRENT_USER\Software\Valve\Steam
     let regKey = new Registry({
       hive: Registry.HKCU,  // open registry hive HKEY_CURRENT_USER
-      key:  '\\Software\\Valve\\Steam'
+      key: '\\Software\\Valve\\Steam'
     })
     regKey.values(function (err, items) {
       let r = false
-      if (err){
-        console.log('ERROR: '+err)
+      if (err) {
+        console.log('ERROR: ' + err)
       }
-      else{
-        for (var i=0; i<items.length && !r; i++){
-          if(items[i].name === REG){
-            r = path.join(items[i].value,WINSTEAMLIB)
+      else {
+        for (var i = 0; i < items.length && !r; i++) {
+          if (items[i].name === REG) {
+            r = path.join(items[i].value, WINSTEAMLIB)
           }
         }
       }
       cb(r)
     })
   }
-  else{
+  else {
     let vdfpath
-    if(os.platform() === 'darwin'){
-      vdfpath = path.join(HOMEDIR,'Library/Application Support/Steam/steamapps/libraryfolders.vdf')
+    if (os.platform() === 'darwin') {
+      vdfpath = path.join(HOMEDIR, 'Library/Application Support/Steam/steamapps/libraryfolders.vdf')
     }
-    else{
-      vdfpath = path.join(HOMEDIR,'.local/share/Steam/steamapps/libraryfolders.vdf')
+    else {
+      vdfpath = path.join(HOMEDIR, '.local/share/Steam/steamapps/libraryfolders.vdf')
     }
     cb(vdfpath)
     return vdfpath
   }
 }
 
-function getLibraryFolders(vdfPath,cb){
+function getLibraryFolders(vdfPath, cb) {
   let folders = []
-  try{
-    fs.readFile(vdfPath,{encoding:'utf8'},function(err,data){
-      if(!err){
+  try {
+    fs.readFile(vdfPath, { encoding: 'utf8' }, function (err, data) {
+      if (!err) {
         let vdfdata = VDF.parse(data)
-        if('LibraryFolders' in vdfdata){
+        if ('LibraryFolders' in vdfdata) {
           vdfdata = vdfdata['LibraryFolders']
           let key
-          for(key in vdfdata){
-            if(!isNaN(key)){
-              folders.push(path.join(vdfdata[key],'steamapps','common'))
+          for (key in vdfdata) {
+            if (!isNaN(key)) {
+              folders.push(path.join(vdfdata[key], 'steamapps', 'common'))
             }
           }
         }
@@ -105,7 +105,7 @@ function getLibraryFolders(vdfPath,cb){
       cb(folders)
     })
   }
-  catch(err){
+  catch (err) {
     cb(folders)
   }
 }
